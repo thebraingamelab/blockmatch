0c58106fd0d8544f51fad0f2dc396f295206fa16
Josh de Leeuw <josh.deleeuw@gmail.com>
2018-09-04 13:49:17 -0400

    newer graphics

--- jspsych-boxes.js
+++ jspsych-boxes.js
@@ -27,18 +27,18 @@ jsPsych.plugins['boxes'] = (function(){
   plugin.trial = function(display_element, trial){
 
     var css_content = "<style>"
-        css_content+= "html, body { background-color: black; }";
-        css_content+= ".boxes-big-container { height: 600px; width: 960px; position: relative; overflow: hidden; background-color: black; border-top: #012BFF 6px solid; border-left: #012BFF 6px solid; border-right: 6px solid #F49661; border-bottom: 6px solid #F49661; }"
-        css_content+= ".boxes-big-container:before { content: ''; position: absolute; pointer-events: none; z-index: 1; top: 0; left: 0; right: 0; bottom: 0; border-top: #F49661 6px solid; border-left: #F49661 6px solid; border-right: 6px solid #012BFF; border-bottom: 6px solid #012BFF; }"
+        //css_content+= "html, body { background-color: black; }";
+        css_content+= ".boxes-big-container { height: 600px; width: 960px; }"
+        //css_content+= ".boxes-big-container:before { content: ''; position: absolute; pointer-events: none; z-index: 1; top: 0; left: 0; right: 0; bottom: 0; border-top: #F49661 6px solid; border-left: #F49661 6px solid; border-right: 6px solid #012BFF; border-bottom: 6px solid #012BFF; }"
         css_content+= "@keyframes change_color {0% { transform: translateY(0px); } 100% {transform: translateY(500px); }} "
         css_content+= ".start_animation {animation: change_color .5s;}"
         css_content+= "@keyframes intro {0% {transform: translateY(-1000px); } 100% { transform:translateY(0px); }}"
         css_content+= ".start_intro_animation {animation: intro .5s;}"
         css_content+= ".around_boxes { position: relative; width:800px; height:350px; top: calc(50% - 175px); left: calc(50% - 400px); } "
-        css_content+= ".keybox { position: absolute; top: 500px;  width: 60px; height: 60px;  margin: 0; text-align:center; }"
-        css_content+= ".keybox p { font-family: 'Overpass'; font-weight: bold; line-height: 70px; margin: 0; padding: 0; font-size: 45px; color:white; }"
-        css_content+= "#Y { left: calc(50% - 70px); background-color: #F49661; }"
-        css_content+= "#N { left: calc(50% + 10px); background-color: #012BFF; }"
+        //css_content+= ".keybox { position: absolute; top: 500px;  width: 60px; height: 60px;  margin: 0; text-align:center; }"
+        //css_content+= ".keybox p { font-family: 'Overpass'; font-weight: bold; line-height: 70px; margin: 0; padding: 0; font-size: 45px; color:white; }"
+        //css_content+= "#Y { left: calc(50% - 70px); background-color: #F49661; }"
+        //css_content+= "#N { left: calc(50% + 10px); background-color: #012BFF; }"
         css_content+= "#inside_box_1 { position:absolute; width:350px; height:350px; left: 0px;  }"
         css_content+= "#inside_box_2 { position:absolute; width:350px; height:350px; right: 0px; } "
         css_content+= ".square:after { content: ''; position:absolute; height:"+Math.round(trial.square_size/3)+"px; width:"+(trial.square_size - trial.square_size*0.1)+"px; top:100%; background-color: #012BFF; left:0px; transform: skewX(45deg); transform-origin: 0 0;}"
@@ -71,13 +71,9 @@ jsPsych.plugins['boxes'] = (function(){
         html_content+= "<div id='inside_box_1'> </div>"
         html_content+= "<div id='inside_box_2'> </div>"
         html_content += "</div>"
-        html_content += "<div class='keybox' id='Y'><p>Y</p></div><div class='keybox' id='N'><p>N</p></div>"
+        //html_content += "<div class='keybox' id='Y'><p>Y</p></div><div class='keybox' id='N'><p>N</p></div>"
         html_content += "</div>"
-        if(trial.dialog!==null){
-          html_content+= "<div class= 'dialog'><div id='dialog_text'> "+trial.dialog[0]+"</div>"
-          html_content+= "<button id= 'dialog_button'>Continue</button>"
-          html_content+= "</div>"
-        }
+        
     display_element.innerHTML = css_content + html_content;
 
     var grid_cols= trial.grid_cols;
@@ -109,73 +105,45 @@ jsPsych.plugins['boxes'] = (function(){
       document.querySelector('#inside_box_2').className = "start_intro_animation";
       // animate only the right box coming from the top of the screen
     }
+
     var correct = null;
+    var rt = null;
 
-    if(trial.dialog!== null){
+    jsPsych.pluginAPI.getKeyboardResponse({
+      callback_function: after_response,
+      valid_responses: ['y','n'],
+      rt_method: 'date',
+      persist: false,
+      allow_held_key: false
+    });
 
-    document.querySelector('#dialog_button').addEventListener("click",function(){
-      if(trial.dialog.length==current_page + 1){
-      document.querySelector('.dialog').remove()
-      addKeyListener()
-    } else {
-      current_page++
-      document.querySelector('#dialog_text').innerHTML=trial.dialog[current_page]
-    }
-    })
-  } else {
-    addKeyListener()
-  }
-    function addKeyListener(){
-      document.addEventListener("keydown", function(e){
+    function after_response(info){
 
-        if (e.keyCode==78){
-          document.querySelector('#inside_box_2').addEventListener("animationend", function(){
-            display_element.innerHTML = "";
-            end();
-          })
-          document.querySelector('#inside_box_2').className = "start_animation";
+      rt = info.rt;
 
-          if(correct_match == 'n'){
+      if (info.key == 78){
+        correct = (correct_match == 'n');
+      } else if(info.key == 89){
+        correct = (correct_match == 'y');
+      }
 
-            document.querySelector('#inside_box_2').insertAdjacentHTML("afterend", '<img src="check.png" style="width:12%; display:block; margin: auto; ">');
-            wAudio.play();
-            correct = true;
-            if(trial.clearboth){
-              document.querySelector('#inside_box_1').className = "start_animation";
-            }
-          }
-          if (correct_match == 'y'){
+      document.querySelector('#inside_box_2').addEventListener("animationend", function(){
+        display_element.innerHTML = "";
+        end();
+      });
+      document.querySelector('#inside_box_2').className = "start_animation";
 
-            document.querySelector('#inside_box_2').insertAdjacentHTML("afterend", '<img src="x.png" style="width:12%; display:block; margin: auto; ">');
-            lAudio.play();
-            correct = false;
-          }
-        } else if( e.keyCode == 89){
-          document.querySelector('#inside_box_2').addEventListener("animationend", function(){
-            display_element.innerHTML = "";
-            end();
-          })
-          document.querySelector('#inside_box_2').className = "start_animation";
-
-          if(correct_match == 'y'){
-            document.querySelector('#inside_box_2').insertAdjacentHTML("afterend", '<img src="check.png" style="width:12%; display:block; margin: auto; ">');
-            wAudio.play();
-            correct = true;
-            if(trial.clearboth){
-              document.querySelector('#inside_box_1').className = "start_animation";
-            }
-          }
-          if(correct_match == 'n'){
-
-            document.querySelector('#inside_box_2').insertAdjacentHTML("afterend", '<img src="x.png" style="width:12%; display:block; margin: auto; ">');
-            lAudio.play();
-            correct = false;
-          }
+      if(correct){
+        document.querySelector('#inside_box_2').insertAdjacentHTML("afterend", '<img src="img/yes.png" style="position: absolute; left: calc(50% - 369px); top: calc(50% - 268px); width: 738px; height: 536px;">');
+        wAudio.play();
+        if(trial.clearboth){
+          document.querySelector('#inside_box_1').className = "start_animation";
         }
-      }, {once:true});
-
+      } else {
+        document.querySelector('#inside_box_2').insertAdjacentHTML("afterend", '<img src="img/no.png" style="position: absolute; left: calc(50% - 268px); top: calc(50% - 268px); width: 536px; height: 536px;">');
+        lAudio.play();
+      }
     }
-
 
     function end(){
       jsPsych.finishTrial({
